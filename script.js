e5774bfc225bef7ab09f99b663c1cf2a307f58e7
Felix Efremov <felixefremov50@gmail.com>
2023-02-14 21:06:38 +0300

    animations

--- script.js
+++ script.js
@@ -17,19 +17,124 @@ document.querySelectorAll(".cabinet__sub-item").forEach((elem) => {
     }
 })
 
+document.querySelectorAll(".menu__go").forEach((elem) => {
+    elem.addEventListener("click", toMonitoring)
+})
+
+document.querySelector(".dropbtn-wrapper").querySelector("img").addEventListener("click", markDrop)
+
 window.onclick = function (event) {
-    if (!event.target.matches('.dropbtn')) {
-        let dropdowns = document.getElementsByClassName("dropdown-content");
-        for (let i = 0; i < dropdowns.length; i++) {
-            let openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains('show')) {
-                openDropdown.classList.remove('show');
-            }
-        }
+    if (!event.target.matches(".dropbtn") && !event.target.matches(".dropbtn-image")) {
+        document.querySelector(".dropdown-content").classList.add("show");
+        document.querySelector(".dropbtn-wrapper").querySelector("img").classList.remove("active");
+    }
+
+    if (event.target.matches(".dropbtn-image")) {
+        markDrop();
     }
 }
 
+
 function markDrop() {
     document.getElementById("dropdown__mark").classList.toggle("show");
+    document.querySelector(".dropbtn-wrapper").querySelector("img").classList.toggle("active");
 }
 
+function setMark(mark) {
+    let word = ["звезда", "звезды", "звезды", "звезды", "звёзд"];
+    let input = document.querySelector(".dropbtn");
+    input.value = `${mark} ${word[+mark - 1]}`
+    input.style.color = "#EEF8FB"
+}
+
+
+function deactiveMenuButtons() {
+    document.querySelector(".menu-buttons__cabinet").classList.remove("active")
+    document.querySelector(".menu-buttons__main").classList.remove("active")
+
+    let line = document.querySelector(".menu-buttons").querySelector(".line");
+    line.style.opacity = 0;
+}
+function toReg() {
+    document.querySelector("main").classList.add("show");
+    document.querySelector(".reg-menu").classList.remove("show");
+    document.querySelector(".reg-menu__entry").classList.add("show");
+    document.querySelector(".reg-menu__registration").classList.remove("show");
+
+    deactiveMenuButtons()
+}
+
+function toEntry() {
+    document.querySelector("main").classList.add("show");
+    document.querySelector(".reg-menu").classList.remove("show");
+    document.querySelector(".reg-menu__registration").classList.add("show");
+    document.querySelector(".reg-menu__entry").classList.remove("show");
+
+    deactiveMenuButtons()
+}
+
+function toMain() {
+    document.querySelector(".reg-menu").classList.add("show")
+    document.querySelector("main").classList.remove("show")
+    document.querySelector(".vuz-all-info").classList.add("show");
+    document.querySelector(".cabinet").classList.add("show")
+    document.querySelector(".menu").classList.remove("show");
+    document.querySelector(".recommendation").classList.remove("show");
+
+    document.querySelector(".menu-buttons__cabinet").classList.remove("active")
+    document.querySelector(".menu-buttons__main").classList.add("active")
+
+    let line = document.querySelector(".menu-buttons").querySelector(".line");
+
+    if (line.style.opacity === "0") {
+        line.style.marginLeft = "0.78125rem"
+        line.style.opacity = 1
+        return;
+    }
+    line.style.animationName = "line-main"
+
+    setTimeout(() => {
+        line.style.marginLeft = "0.78125rem"
+    }, 1190)
+}
+
+function toCabinet() {
+    document.querySelector(".reg-menu").classList.add("show")
+    document.querySelector("main").classList.remove("show")
+    document.querySelector(".menu").classList.add("show");
+    document.querySelector(".vuz-all-info").classList.add("show");
+    document.querySelector(".recommendation").classList.add("show");
+    document.querySelector(".cabinet").classList.remove("show")
+
+    document.querySelector(".menu-buttons__main").classList.remove("active")
+    document.querySelector(".menu-buttons__cabinet").classList.add("active")
+
+    let line = document.querySelector(".menu-buttons").querySelector(".line")
+
+    if (line.style.opacity === "0") {
+        line.style.marginLeft = "8.125rem"
+        line.style.opacity = 1
+        return;
+    }
+    line.style.animationName = "line-cabinet"
+
+    setTimeout(() => {
+        line.style.marginLeft = "8.125rem"
+    }, 1190)
+}
+
+function toMonitoring() {
+    document.querySelector(".reg-menu").classList.add("show")
+    document.querySelector(".cabinet").classList.add("show")
+    document.querySelector(".menu").classList.add("show");
+    document.querySelector(".recommendation").classList.add("show");
+    document.querySelector(".vuz-all-info").classList.remove("show");
+
+    deactiveMenuButtons()
+
+    window.scrollTo({
+        left: 0,
+        top: 0,
+        behavior: "smooth"
+    })
+}
